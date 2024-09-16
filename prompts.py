928b20082d21eb1503ecee72831eb3bf0fb8489b
breadbread1984 <breadbread1984@163.com>
2024-09-16 04:40:04 +0000

    update

--- prompts.py
+++ prompts.py
@@ -4,7 +4,7 @@ from langchain_core.prompts.prompt import PromptTemplate
 
 def experimental_template(tokenizer):
   messages = [
-    {'role': 'system', 'content': 'Given a full text of a paper. Please extract the original text of the experimental part of the paper. If the experimental part is not present in the paper, just return "<no experimental>".'},
+    {'role': 'system', 'content': 'Given a full text of a paper. Please return the original text of the experimental part of the paper. If the experimental part is not present in the paper, just return "<no experimental>".'},
     {'role': 'user', 'content': 'the full text:\n\n{text}'}
   ]
   prompt = tokenizer.apply_chat_template(messages, tokenize = False, add_generating_prompt = True)
