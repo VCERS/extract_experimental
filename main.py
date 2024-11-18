6b66d48d836118f50caae5481510895f35aa9052
breadbread1984 <breadbread1984@163.com>
2024-11-18 01:56:12 +0000

    update

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from os.path import splitext, join, exists
 from absl import flags, app
 from tqdm import tqdm
 import json
-#from langchain_community.document_loaders import UnstructuredMarkdownLoader
+from transformers import AutoTokenizer
 from langchain.document_loaders import UnstructuredMarkdownLoader, TextLoader
-from models import Llama3, Qwen2
+from models import TGI
 from chains import experimental_chain
 
 FLAGS = flags.FLAGS
@@ -16,14 +16,13 @@ FLAGS = flags.FLAGS
 def add_options():
   flags.DEFINE_string('input_dir', default = None, help = 'path to input directory')
   flags.DEFINE_string('output_dir', default = 'processed', help = 'path to output directory')
-  flags.DEFINE_enum('model', default = 'llama3', enum_values = {'llama3', 'qwen2'}, help = 'model to use')
+  flags.DEFINE_string('host', default = 'http://localhost:8080/generate', help = 'url to TGI')
 
 def main(unused_argv):
   if exists(FLAGS.output_dir): rmtree(FLAGS.output_dir)
   mkdir(FLAGS.output_dir)
-  tokenizer, llm = {
-    'llama3': Llama3,
-    'qwen2': Qwen2}[FLAGS.model](True)
+  llm = TGI(FLAGS.host)
+  tokenizer = AutoTokenizer.from_pretrained('Qwen/Qwen2.5-7B-Instruct')
   exp_chain = experimental_chain(llm, tokenizer)
   for root, dirs, files in tqdm(walk(FLAGS.input_dir)):
     for f in files:
